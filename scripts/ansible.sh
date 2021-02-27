2762bfd93adc38db3f66efc36c98e6b628b954ca
Kishan <kishanvekaria123@gmail.com>
2021-02-27 19:03:53 +0000

    ansible file changed

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-mkdir -p ~/.local/bin
-echo 'PATH=$PATH:~/.local/bin' >> ~/.bashrc
-source ~/.bashrc
-pip3 install --user ansible
+cd ~/.jenkins/workspace/project2pipeline
 ansible-playbook -i inventory playbook.yaml 
