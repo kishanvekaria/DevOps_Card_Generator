704b8550db3317886f34aa9e71ea5008c1f1e490
Kishan <kishanvekaria123@gmail.com>
2021-02-27 19:26:45 +0000

    deploy edited 1

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-ssh 35.233.174.240 << EOF
-scp /home/jenkins/.jenkins/workspace/project2pipeline/docker-compose.yaml /home/jenkins/
+<< EOF
+scp /home/jenkins/.jenkins/workspace/project2pipeline/docker-compose.yaml jenkins@35.233.174.240:docker-compose.yaml
+ssh 35.233.174.240
 export SEC_KEY=${SEC_KEY} 
 export DB_URI=${DB_URI} 
 docker stack deploy --compose-file docker-compose.yaml cardgen
