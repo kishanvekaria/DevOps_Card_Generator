8a4c933aa4730a8159e3f19288204ff13d79bdaa
Kishan <kishanvekaria123@gmail.com>
2021-02-27 17:29:51 +0000

    build and push

--- scripts/buildandpush.sh
+++ scripts/buildandpush.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+sudo apt update
+sudo apt install -y curl jq
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
-ls
+sudo usermod -aG docker $(whoami)
+sudo usermod -aG docker jenkins
+newgrp docker
+
+docker-compose down --rmi all
+docker-compose build
+sudo docker login
+docker-compose push
