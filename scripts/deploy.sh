e97489d5562c458679c92362b1e32419f9e48ee3
Kishan <kishanvekaria123@gmail.com>
2021-02-27 19:15:41 +0000

    user changed in inventory and deploy added

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-ls
+ssh 35.233.174.240 << EOF
+scp /home/jenkins/.jenkins/workspace/project2pipeline/docker-compose.yaml /home/jenkins/
+export SEC_KEY=${SEC_KEY} 
+export DB_URI=${DB_URI} 
+docker stack deploy --compose-file docker-compose.yaml cardgen
+EOF
