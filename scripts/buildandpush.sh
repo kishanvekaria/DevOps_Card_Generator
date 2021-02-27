fbd79e69b96fb86b657a621ab285fc7ed3f7dcc2
Kishan <kishanvekaria123@gmail.com>
2021-02-27 18:22:47 +0000

    env added to jenkins user

--- scripts/buildandpush.sh
+++ scripts/buildandpush.sh
@@ -2,8 +2,8 @@
 sudo chmod 666 /var/run/docker.sock
 
 docker-compose down --rmi all
-docker-compose build
-sudo docker login
 export DB_URI=${DB_URI} 
 export SEC_KEY=${SEC_KEY} 
+docker-compose build
+sudo docker login
 sudo docker-compose push
