7124700ef05789e8c834a1f659093faead5795fb
Kishan <kishanvekaria123@gmail.com>
2021-02-27 18:33:31 +0000

    no export env

--- scripts/buildandpush.sh
+++ scripts/buildandpush.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 sudo chmod 666 /var/run/docker.sock
 
-docker-compose down --rmi all
-export DB_URI=${DB_URI} 
-export SEC_KEY=${SEC_KEY} 
+docker-compose down --rmi all 
 docker-compose build
 sudo docker login
 sudo docker-compose push
