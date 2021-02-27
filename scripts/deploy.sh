60114c97fdce76a7d8ed16cc711c8b91bf177dec
Kishan <kishanvekaria123@gmail.com>
2021-02-27 19:44:46 +0000

    ssh -i added

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 << EOF
 scp /home/jenkins/.jenkins/workspace/project2pipeline/docker-compose.yaml jenkins@35.233.174.240:docker-compose.yaml
-ssh 35.233.174.240
+ssh -i ./ssh/id_rsa jenkins@35.233.174.240
 export SEC_KEY=${SEC_KEY} 
 export DB_URI=${DB_URI} 
 docker stack deploy --compose-file docker-compose.yaml cardgen
