a749df769b3660c3f004d531ff19d8f445250253
Kishan <kishanvekaria123@gmail.com>
2021-02-27 18:11:41 +0000

    position of env variables edit 2

--- scripts/buildandpush.sh
+++ scripts/buildandpush.sh
@@ -3,7 +3,7 @@ sudo chmod 666 /var/run/docker.sock
 
 docker-compose down --rmi all
 docker-compose build
+sudo docker login
 export DB_URI=${DB_URI} 
 export SEC_KEY=${SEC_KEY} 
-sudo docker login
 sudo docker-compose push
