5c03cf625cdbb1b393e097713c3b01412a559918
Kishan <kishanvekaria123@gmail.com>
2021-02-28 17:38:35 +0000

    cleared unecessary comments

--- service3/app.py
+++ service3/app.py
@@ -12,20 +12,6 @@ def card_suit():
     return Response(first_suit, mimetype="text/plain")
 
 
-#jason if necessary later    
-#
-#@app.route('/card_suit', methods= ['GET'])
-#def card_suit():
-#    suits = ["Spades", "Diamonds", "Hearts", "Clubs"]
-#    first_suit= random.choices(suits)
-#    second_suit= random.choices(suits)
-#    diction = {
-#        "suit1": first_suit,
-#        "suit2": second_suit
-#    }
-#    res_suit= json.dumps(diction)
-#    return res_suit
-
 
 if __name__=="__main__":
     app.run(debug=True, host='0.0.0.0', port=5002)
