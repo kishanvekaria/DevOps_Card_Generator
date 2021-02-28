75338c22e2f9d17d7d92beb63335be719821fd66
Kishan <kishanvekaria123@gmail.com>
2021-02-28 18:11:33 +0000

    changed testing to french

--- service1/tests/test_unit1.py
+++ service1/tests/test_unit1.py
@@ -12,9 +12,9 @@ class TestBase(TestCase):
 class TestResponse(TestBase):
     def test_card_face_page(self):
         with patch("requests.get") as g:
-            g.return_value.text = "King"
+            g.return_value.text = "Roi"
             response = self.client.get(url_for("index"))
             self.assertEqual(response.status_code, 200)
-            self.assertIn(b'King', response.data)
+            self.assertIn(b'Roi', response.data)
 
         
