28278600e5fe8d40d69f9ade196506fbfd984ce1
Kishan <kishanvekaria123@gmail.com>
2021-02-28 13:30:03 +0000

    tests edited

--- service1/tests/test_unit1.py
+++ service1/tests/test_unit1.py
@@ -1,17 +1,20 @@
 from unittest.mock import patch
 from flask import url_for, Response, request
 from flask_testing import TestCase
-
-from app import app
+from os import getenv
+from app import app, Storage
 
 class TestBase(TestCase):
     def create_app(self):
         return app
+    
 
 class TestResponse(TestBase):
     def test_card_face_page(self):
         with patch("requests.get") as g:
             g.return_value.text = "King"
-
             response = self.client.get(url_for("index"))
+            self.assertEqual(response.status_code, 200)
             self.assertIn(b'King', response.data)
+
+        
