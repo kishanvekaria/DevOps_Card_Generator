fcd32b53da06b0e77cf408b9d792dcdf6e657c0c
Kishan <kishanvekaria123@gmail.com>
2021-02-28 18:23:12 +0000

    english version commit

--- service3/app.py
+++ service3/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, Response, request, jsonify, make_response
 import requests
 import random
-#import json
+
 app = Flask(__name__)
 
 @app.route('/card_suit', methods= ['GET'])
